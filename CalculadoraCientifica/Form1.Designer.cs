b1f153aee04a5722be64638dc3f9e16424e19387
Evelin Lizeth Hernández <21620248@tlaxiaco.tecnm.mx>
2025-03-08 19:39:54 -0600

    inserccion de button

--- CalculadoraCientifica/Form1.Designer.cs
+++ CalculadoraCientifica/Form1.Designer.cs
@@ -28,20 +28,306 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtPantalla = new System.Windows.Forms.TextBox();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.btnBorarTodo = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnPunto = new System.Windows.Forms.Button();
+            this.btnN3 = new System.Windows.Forms.Button();
+            this.btnN6 = new System.Windows.Forms.Button();
+            this.btnN9 = new System.Windows.Forms.Button();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.btnN2 = new System.Windows.Forms.Button();
+            this.btnN5 = new System.Windows.Forms.Button();
+            this.btnN8 = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnN0 = new System.Windows.Forms.Button();
+            this.btnN1 = new System.Windows.Forms.Button();
+            this.btnN4 = new System.Windows.Forms.Button();
+            this.btnN7 = new System.Windows.Forms.Button();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPorcentaje = new System.Windows.Forms.Button();
+            this.BtnPotencia = new System.Windows.Forms.Button();
+            this.btnRaiz = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtPantalla
+            // 
+            this.txtPantalla.Location = new System.Drawing.Point(19, 79);
+            this.txtPantalla.Name = "txtPantalla";
+            this.txtPantalla.ReadOnly = true;
+            this.txtPantalla.Size = new System.Drawing.Size(345, 20);
+            this.txtPantalla.TabIndex = 38;
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIgual.Location = new System.Drawing.Point(299, 256);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(71, 132);
+            this.btnIgual.TabIndex = 37;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            // 
+            // btnBorarTodo
+            // 
+            this.btnBorarTodo.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorarTodo.Location = new System.Drawing.Point(299, 210);
+            this.btnBorarTodo.Name = "btnBorarTodo";
+            this.btnBorarTodo.Size = new System.Drawing.Size(71, 40);
+            this.btnBorarTodo.TabIndex = 36;
+            this.btnBorarTodo.Text = "EC";
+            this.btnBorarTodo.UseVisualStyleBackColor = true;
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivision.Location = new System.Drawing.Point(299, 164);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(71, 40);
+            this.btnDivision.TabIndex = 35;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            // 
+            // btnPunto
+            // 
+            this.btnPunto.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPunto.Location = new System.Drawing.Point(206, 348);
+            this.btnPunto.Name = "btnPunto";
+            this.btnPunto.Size = new System.Drawing.Size(71, 40);
+            this.btnPunto.TabIndex = 34;
+            this.btnPunto.Text = ".";
+            this.btnPunto.UseVisualStyleBackColor = true;
+            // 
+            // btnN3
+            // 
+            this.btnN3.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN3.Location = new System.Drawing.Point(206, 302);
+            this.btnN3.Name = "btnN3";
+            this.btnN3.Size = new System.Drawing.Size(71, 40);
+            this.btnN3.TabIndex = 33;
+            this.btnN3.Text = "3";
+            this.btnN3.UseVisualStyleBackColor = true;
+            // 
+            // btnN6
+            // 
+            this.btnN6.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN6.Location = new System.Drawing.Point(206, 256);
+            this.btnN6.Name = "btnN6";
+            this.btnN6.Size = new System.Drawing.Size(71, 40);
+            this.btnN6.TabIndex = 32;
+            this.btnN6.Text = "6";
+            this.btnN6.UseVisualStyleBackColor = true;
+            // 
+            // btnN9
+            // 
+            this.btnN9.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN9.Location = new System.Drawing.Point(206, 210);
+            this.btnN9.Name = "btnN9";
+            this.btnN9.Size = new System.Drawing.Size(71, 40);
+            this.btnN9.TabIndex = 31;
+            this.btnN9.Text = "9";
+            this.btnN9.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicacion.Location = new System.Drawing.Point(206, 164);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(71, 40);
+            this.btnMultiplicacion.TabIndex = 30;
+            this.btnMultiplicacion.Text = "*";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            // 
+            // btnN2
+            // 
+            this.btnN2.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN2.Location = new System.Drawing.Point(115, 302);
+            this.btnN2.Name = "btnN2";
+            this.btnN2.Size = new System.Drawing.Size(71, 40);
+            this.btnN2.TabIndex = 29;
+            this.btnN2.Text = "2";
+            this.btnN2.UseVisualStyleBackColor = true;
+            // 
+            // btnN5
+            // 
+            this.btnN5.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN5.Location = new System.Drawing.Point(115, 256);
+            this.btnN5.Name = "btnN5";
+            this.btnN5.Size = new System.Drawing.Size(71, 40);
+            this.btnN5.TabIndex = 28;
+            this.btnN5.Text = "5";
+            this.btnN5.UseVisualStyleBackColor = true;
+            // 
+            // btnN8
+            // 
+            this.btnN8.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN8.Location = new System.Drawing.Point(115, 210);
+            this.btnN8.Name = "btnN8";
+            this.btnN8.Size = new System.Drawing.Size(71, 40);
+            this.btnN8.TabIndex = 27;
+            this.btnN8.Text = "8";
+            this.btnN8.UseVisualStyleBackColor = true;
+            // 
+            // btnResta
+            // 
+            this.btnResta.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResta.Location = new System.Drawing.Point(115, 164);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(71, 40);
+            this.btnResta.TabIndex = 26;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = true;
+            // 
+            // btnN0
+            // 
+            this.btnN0.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN0.Location = new System.Drawing.Point(25, 347);
+            this.btnN0.Name = "btnN0";
+            this.btnN0.Size = new System.Drawing.Size(167, 41);
+            this.btnN0.TabIndex = 25;
+            this.btnN0.Text = "0";
+            this.btnN0.UseVisualStyleBackColor = true;
+            // 
+            // btnN1
+            // 
+            this.btnN1.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN1.Location = new System.Drawing.Point(25, 302);
+            this.btnN1.Name = "btnN1";
+            this.btnN1.Size = new System.Drawing.Size(71, 40);
+            this.btnN1.TabIndex = 24;
+            this.btnN1.Text = "1";
+            this.btnN1.UseVisualStyleBackColor = true;
+            // 
+            // btnN4
+            // 
+            this.btnN4.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN4.Location = new System.Drawing.Point(25, 256);
+            this.btnN4.Name = "btnN4";
+            this.btnN4.Size = new System.Drawing.Size(71, 40);
+            this.btnN4.TabIndex = 23;
+            this.btnN4.Text = "4";
+            this.btnN4.UseVisualStyleBackColor = true;
+            // 
+            // btnN7
+            // 
+            this.btnN7.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnN7.Location = new System.Drawing.Point(25, 210);
+            this.btnN7.Name = "btnN7";
+            this.btnN7.Size = new System.Drawing.Size(71, 40);
+            this.btnN7.TabIndex = 22;
+            this.btnN7.Text = "7";
+            this.btnN7.UseVisualStyleBackColor = true;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(25, 164);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(71, 40);
+            this.btnSuma.TabIndex = 21;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(299, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 40);
+            this.button1.TabIndex = 43;
+            this.button1.Text = "/";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnPorcentaje
+            // 
+            this.btnPorcentaje.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPorcentaje.Location = new System.Drawing.Point(206, 118);
+            this.btnPorcentaje.Name = "btnPorcentaje";
+            this.btnPorcentaje.Size = new System.Drawing.Size(71, 40);
+            this.btnPorcentaje.TabIndex = 42;
+            this.btnPorcentaje.Text = "%";
+            this.btnPorcentaje.UseVisualStyleBackColor = true;
+            // 
+            // BtnPotencia
+            // 
+            this.BtnPotencia.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPotencia.Location = new System.Drawing.Point(115, 118);
+            this.BtnPotencia.Name = "BtnPotencia";
+            this.BtnPotencia.Size = new System.Drawing.Size(71, 40);
+            this.BtnPotencia.TabIndex = 41;
+            this.BtnPotencia.Text = "^";
+            this.BtnPotencia.UseVisualStyleBackColor = true;
+            // 
+            // btnRaiz
+            // 
+            this.btnRaiz.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRaiz.Location = new System.Drawing.Point(25, 118);
+            this.btnRaiz.Name = "btnRaiz";
+            this.btnRaiz.Size = new System.Drawing.Size(71, 40);
+            this.btnRaiz.TabIndex = 40;
+            this.btnRaiz.Text = "√";
+            this.btnRaiz.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPorcentaje);
+            this.Controls.Add(this.BtnPotencia);
+            this.Controls.Add(this.btnRaiz);
+            this.Controls.Add(this.txtPantalla);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnBorarTodo);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnPunto);
+            this.Controls.Add(this.btnN3);
+            this.Controls.Add(this.btnN6);
+            this.Controls.Add(this.btnN9);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.btnN2);
+            this.Controls.Add(this.btnN5);
+            this.Controls.Add(this.btnN8);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnN0);
+            this.Controls.Add(this.btnN1);
+            this.Controls.Add(this.btnN4);
+            this.Controls.Add(this.btnN7);
+            this.Controls.Add(this.btnSuma);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPantalla;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Button btnBorarTodo;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Button btnPunto;
+        private System.Windows.Forms.Button btnN3;
+        private System.Windows.Forms.Button btnN6;
+        private System.Windows.Forms.Button btnN9;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Button btnN2;
+        private System.Windows.Forms.Button btnN5;
+        private System.Windows.Forms.Button btnN8;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnN0;
+        private System.Windows.Forms.Button btnN1;
+        private System.Windows.Forms.Button btnN4;
+        private System.Windows.Forms.Button btnN7;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPorcentaje;
+        private System.Windows.Forms.Button BtnPotencia;
+        private System.Windows.Forms.Button btnRaiz;
     }
 }
 
